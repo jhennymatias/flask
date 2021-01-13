94ddeba2b4a8a9ba6322e40f6e2d4da168b304e3
jhennymatias <jhennifercristinematias@gmail.com>
2021-01-13 16:50:33 -0300

    aula 9

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.models.forms import LoginForm
 
 @app.route("/index/<user>")
 @app.route("/", defaults={"user":None})
@@ -10,3 +11,12 @@ def index(user):
 
 def test():
       return render_template('base.html')
+
+@app.route('/login', methods=["GET", "POST"])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    return render_template('login.html', form = form)
